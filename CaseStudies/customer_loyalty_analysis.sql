3f919f4d184fa615c00faa5d97cfd9a4d2efd985
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-24 17:49:42 +0300

    Update customer_loyalty_analysis.sql

--- CaseStudies/customer_loyalty_analysis.sql
+++ CaseStudies/customer_loyalty_analysis.sql
@@ -1,12 +1,17 @@
 -- In order to analyze customer loyalty,
 -- rank customers based on the average days between their orders
-
-SELECT 
-	OrderID,
+SELECT
 	CustomerID,
-	OrderDate CurrentOrder,
-	LEAD(OrderDate) OVER (PARTITION BY CustomerID ORDER BY OrderDate ) NextOrder,
-	DATEDIFF(day, OrderDate , LEAD(OrderDate) OVER (PARTITION BY CustomerID ORDER BY OrderDate )) DaysUntilNextOrder
+	AVG(DaysUntilNextOrder) AvgDays
+FROM(
+	SELECT 
+		OrderID,
+		CustomerID,
+		OrderDate CurrentOrder,
+		LEAD(OrderDate) OVER (PARTITION BY CustomerID ORDER BY OrderDate ) NextOrder,
+		DATEDIFF(day, OrderDate , LEAD(OrderDate) OVER (PARTITION BY CustomerID ORDER BY OrderDate )) DaysUntilNextOrder
 
-FROM Sales.Orders
-ORDER BY CustomerID, OrderDate
+	FROM Sales.Orders
+)t
+
+GROUP BY CustomerID
