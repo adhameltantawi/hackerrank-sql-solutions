d7f4eb03b4a504bc3cae1f67ba431ff0345ae49a
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-24 19:29:21 +0300

    Update product_sales_extremes.sql

--- CaseStudies/product_sales_extremes.sql
+++ CaseStudies/product_sales_extremes.sql
@@ -9,4 +9,18 @@ SELECT
 	LAST_VALUE (Sales) OVER (PARTITION BY ProductID ORDER BY Sales DESC 
 	ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) LowestSales
 
+FROM Sales.Orders;
+
+
+-- Find the lowest and highest sales for each product
+
+
+SELECT
+	OrderID,
+	ProductID,
+	Sales,
+	FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales DESC) HighestSales,
+	FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales ASC ) LowestSales
+
 FROM Sales.Orders
+
