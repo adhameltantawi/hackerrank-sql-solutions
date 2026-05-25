11da2ee2cf136735ea8a8ec0db91a5164a82e48e
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-25 06:13:39 +0300

    Create customer_analysis.sql

---
+++ CaseStudies/customer_analysis.sql
@@ -0,0 +1,10 @@
+-- Find the Average Scores of Customers
+
+SELECT
+    CustomerID,
+    LastName,
+    Score,
+    COALESCE(Score, 0) AS CustomerScore,
+    AVG(Score) OVER () AS AvgScore,
+    AVG(COALESCE(Score, 0)) OVER () AS AvgScoreWithoutNull
+FROM Sales.Customers
