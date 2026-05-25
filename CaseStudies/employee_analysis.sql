4e75cd0e98da1194038433b3e637e1326edfd51a
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-25 06:14:11 +0300

    Create employee_analysis.sql

---
+++ CaseStudies/employee_analysis.sql
@@ -0,0 +1,9 @@
+-- Show the employees who have the highest salaries
+
+SELECT *
+FROM (
+	SELECT *,
+		   MAX(Salary) OVER() AS HighestSalary
+	FROM Sales.Employees
+) t
+WHERE Salary = HighestSalary
