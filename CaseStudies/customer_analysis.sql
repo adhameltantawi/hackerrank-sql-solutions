57d48cd5a2be9c03c8e48721062623d342c02f22
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-25 07:13:38 +0300

    Update customer_analysis.sql

--- CaseStudies/customer_analysis.sql
+++ CaseStudies/customer_analysis.sql
@@ -8,3 +8,16 @@ SELECT
     AVG(Score) OVER () AS AvgScore,
     AVG(COALESCE(Score, 0)) OVER () AS AvgScoreWithoutNull
 FROM Sales.Customers
+
+
+-- Rank customers based on their total amount of sales
+
+SELECT 
+	*,
+	RANK() OVER(ORDER BY SumOfSales) Rank
+FROM(
+SELECT
+	CustomerID,
+	SUM(Sales) SumOfSales
+FROM SALES.Orders
+GROUP BY CustomerID)t
