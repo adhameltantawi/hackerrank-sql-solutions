243615052b534213857376456322628ae965a16a
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-27 19:16:25 +0300

    Create data_modeling_and_views.sql

---
+++ CaseStudies/data_modeling_and_views.sql
@@ -0,0 +1,29 @@
+-- Provide a view that combines details from orders, products, customers, and employees.
+CREATE VIEW Sales.V_Order_Details AS 
+(
+
+SELECT 
+	O.OrderID,
+	O.OrderDate,
+	P.Product,
+	P.Category,
+	COALESCE(C.FirstName + ' ', '') + COALESCE(C.LastName,'') AS CustomerName,
+	C.Country AS CustomerCountry,
+	COALESCE(E.FirstName + ' ', '') + COALESCE(E.LastName,'') AS SalesName,
+	E.Department,
+	O.Sales,
+	O.Quantity
+
+FROM Sales.Orders AS O
+LEFT JOIN Sales.Products AS P
+ON O.ProductID = P.ProductID
+LEFT JOIN Sales.Customers AS C
+ON C.CustomerID = O.CustomerID
+LEFT JOIN Sales.Employees AS E
+ON O.SalesPersonID = E.EmployeeID
+
+)
+
+
+SELECT * 
+FROM Sales.V_Order_Details
