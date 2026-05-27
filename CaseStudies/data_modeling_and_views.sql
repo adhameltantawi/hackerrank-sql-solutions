5b5e863f0423176ee71099de85fc99c1435b03d4
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-27 19:25:28 +0300

    Update data_modeling_and_views.sql

--- CaseStudies/data_modeling_and_views.sql
+++ CaseStudies/data_modeling_and_views.sql
@@ -28,3 +28,37 @@ ON O.SalesPersonID = E.EmployeeID
 
 SELECT * 
 FROM Sales.V_Order_Details
+
+
+-- Provide a view for the EU Sales Team
+-- that combines details from all tables 
+-- and excludes data related to the USA.
+
+
+CREATE VIEW Sales.V_Order_DetailS_EU AS 
+(
+SELECT 
+	O.OrderID,
+	O.OrderDate,
+	P.Product,
+	P.Category,
+	COALESCE(C.FirstName + ' ', '') + COALESCE(C.LastName,'') AS CustomerName,
+	C.Country AS CustomerCountry,
+	COALESCE(E.FirstName + ' ', '') + COALESCE(E.LastName,'') AS SalesName,
+	E.Department,
+	O.Sales,
+	O.Quantity
+
+FROM Sales.Orders AS O
+LEFT JOIN Sales.Products AS P
+ON O.ProductID = P.ProductID
+LEFT JOIN Sales.Customers AS C
+ON C.CustomerID = O.CustomerID
+LEFT JOIN Sales.Employees AS E
+ON O.SalesPersonID = E.EmployeeID
+
+WHERE C.Country != 'USA'
+)
+
+SELECT *
+FROM Sales.V_Order_DetailS_EU
