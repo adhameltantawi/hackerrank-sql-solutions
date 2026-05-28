4aa981a1b8d4453d253adc9f8b481863ab0264eb
Adham Eltantawi <adhameltantawi@gmail.com>
2026-05-28 20:55:38 +0300

    styling

--- CaseStudies/stored_procedures.sql
+++ CaseStudies/stored_procedures.sql
@@ -1,60 +1,61 @@
--- For German Customers Find the Total Number of Customers and the Average Score
--- And Find the total Nr. of Orders and Total Sales
+-- Calcualte Total Customers and Average Score for specific Country
+-- Calculate Total Number of Orders and Total Sales for specific Country
 
 
 CREATE PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA'
 AS 
-BEGIN
-BEGIN TRY
+	BEGIN
+	BEGIN TRY
 
-DECLARE @TotalCustomers INT, @AvgScore FLOAT;
+		DECLARE @TotalCustomers INT, @AvgScore FLOAT;
 
--- Prepare & Cleanup Data
-
-
-IF EXISTS(SELECT 1 FROM Sales.Customers WHERE SCORE IS NULL AND Country = @Country)
-BEGIN
-	PRINT('Updating NULL Scores to 0');
-	UPDATE Sales.Customers
-	SET Score = 0
-	WHERE Score IS NULL AND Country = @Country;
-END
-
-ELSE
-BEGIN
-	PRINT('No NULL Scores found')
-END;
+		-- Prepare & Cleanup Data
 
 
+		IF EXISTS(SELECT 1 FROM Sales.Customers WHERE SCORE IS NULL AND Country = @Country)
+		BEGIN
+			PRINT('Updating NULL Scores to 0');
+			UPDATE Sales.Customers
+			SET Score = 0
+			WHERE Score IS NULL AND Country = @Country;
+		END
 
--- Generating Reports
-SELECT 
-	@TotalCustomers = COUNT(*),
-	@AvgScore = AVG(Score) 
-FROM Sales.Customers
-WHERE Country = @Country;
+		ELSE
+		BEGIN
+			PRINT('No NULL Scores found')
+		END;
 
-PRINT 'Total Customers from ' + @Country+ ': ' +  CAST(@TotalCustomers AS VARCHAR);
-PRINT 'Average Score from ' + @Country+ ': ' +  CAST(@AvgScore AS VARCHAR) ;
 
-SELECT
-	COUNT(OrderID) TotalOrders,
-	SUM(Sales) TotalSales
+		
+		-- Generating Reports
+		SELECT 
+			@TotalCustomers = COUNT(*),
+			@AvgScore = AVG(Score) 
+		FROM Sales.Customers
+		WHERE Country = @Country;
 
-FROM Sales.Orders AS O
-JOIN Sales.Customers AS C
-ON O.CustomerID = C.CustomerID
+		PRINT 'Total Customers from ' + @Country+ ': ' +  CAST(@TotalCustomers AS VARCHAR);
+		PRINT 'Average Score from ' + @Country+ ': ' +  CAST(@AvgScore AS VARCHAR) ;
 
-WHERE Country = @country; 
+		SELECT
+			COUNT(OrderID) TotalOrders,
+			SUM(Sales) TotalSales
 
-END TRY
-BEGIN CATCH
-	PRINT('An error occured');
-	PRINT('Error Message: ' + ERROR_MESSAGE());
-	PRINT('Error Number: ' + CAST(ERROR_NUMBER() AS VARCHAR));
-	PRINT('Error Line: ' + CAST(ERROR_LINE() AS VARCHAR))
-	PRINT('Error Procedure: ' + ERROR_PROCEDURE())
-END CATCH
+		FROM Sales.Orders AS O
+		JOIN Sales.Customers AS C
+		ON O.CustomerID = C.CustomerID
+
+		WHERE Country = @country; 
+
+	END TRY
+	BEGIN CATCH
+		-- Error Handling 
+		PRINT('An error occured');
+		PRINT('Error Message: ' + ERROR_MESSAGE());
+		PRINT('Error Number: ' + CAST(ERROR_NUMBER() AS VARCHAR));
+		PRINT('Error Line: ' + CAST(ERROR_LINE() AS VARCHAR))
+		PRINT('Error Procedure: ' + ERROR_PROCEDURE())
+	END CATCH
 
 END
 GO
